b055ddd4aebe13d44a2a3ffbf80b6a4670797b40
Filipe Silva <68608851+FilipeOSilva@users.noreply.github.com>
2025-04-12 10:21:50 -0300

    Create install.bash
    Apps install in ubuntu

---
+++ install.bash
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+sudo apt install git \
+		 curl \
+		 vim \
+		 meld \
+		 build-essential \
+		 dkms \
+		 tcpdump \
+		 pipx \
+		 pip \
+		 tmux \
+		 gnome-shell-extension-manager \
+		 gir1.2-gtop-2.0 \
+		 lm-sensors
